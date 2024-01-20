ad6bb532b5f375f56da2ac9d821719eb9419c536
zeid <zeidhassan4@gmail.com>
2024-01-20 12:14:32 +0300

    Project Done

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+     //How to access input elements
 const inputBox = document.querySelector('.input-box')
+    // How to access search result elements
 const searchBtn = document.getElementById('searchBtn')
+   // How to access weather-icon content
 const weatherIcon = document.querySelector('.weather-icon')
+   // How to access temperature content
 const temperature = document.querySelector('.temperature')
+  // How to access description content
 const description = document.querySelector('.description')
+  // How to access humidity content
 const humidity = document.querySelector('.humidity')
+  // How to access wind Speed content
 const windSpeed = document.querySelector('.wind')
+  // How to access error content
 const locationNotFound = document.querySelector('.location-not-found')
+  // How to access weather-body content
 const weatherBody = document.querySelector('.weather')
 
 
-
+  // Creating a callback function
 async function checkWeather(city) {
+    // My public Api Key
     const APIKey = "8c1146caa555e7ad9386e0f9ae70abed";
+    // The url for my Api Key
     const url =    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
 
-  
+      //sending a request to the api and waits for response
     const weather_data = await fetch(`${url}`)
     .then(response => response.json());
 
@@ -66,6 +78,14 @@ async function checkWeather(city) {
           
     }
 }
-searchBtn.addEventListener('click', () => {
+  inputBox.addEventListener('keydown', (event) => {
+    // Check if the Enter key is pressed (key code 13)
+    if (event.key === 'Enter') {
+      checkWeather(inputBox.value);
+    }
+  });
+
+  searchBtn.addEventListener('click', () => {
     checkWeather(inputBox.value);
-})
+  })
+}) 
